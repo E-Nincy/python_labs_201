d0574735ba8416b38b9a15eb7e7cffc6c856aa99
E-Nincy <escotonincy10@outlook.es>
2025-06-04 15:21:21 +0800

    add text

--- 02_more-datatypes/02_01_converter.py
+++ 02_more-datatypes/02_01_converter.py
@@ -1,6 +1,15 @@
 # Convert a string to a tuple and print out the result.
 # What do you see?
+# Asnwer: I see a tuple of characters, = ('c', 'o', 'd', 'i', 'n', 'g', 'n', 'o', 'm', 'a', 'd', 's')
+
 # What happens if you try to iterate over your tuple of characters?
+# Answer: I will get a letter per line
+
 # Do you notice any difference to iterating over the string?
+# Answer: No visual difference in the output, but there is a difference in data types.
+
 
 string = "codingnomads"
+
+my_tuple = tuple(string)
+print(my_tuple)             # Output: ('c', 'o', 'd', 'i', 'n', 'g', 'n', 'o', 'm', 'a', 'd', 's')
