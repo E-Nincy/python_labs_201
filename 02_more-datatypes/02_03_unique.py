f648d9a6f11870c4a43748a5714d0dc1d1f6e94f
E-Nincy <escotonincy10@outlook.es>
2025-06-05 17:30:38 +0800

    add text

--- 02_more-datatypes/02_03_unique.py
+++ 02_more-datatypes/02_03_unique.py
@@ -3,3 +3,8 @@
 #
 # list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
 # unique_list = [55, 'hi', 4, 13]
+
+my_list = [5, 6, 9, 'Love', 9, 2, 13, 6, 5]
+
+unique_list = [x for x in my_list if my_list.count(x) == 1]
+print(unique_list)
