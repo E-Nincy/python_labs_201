c313f20dfcfe2e0ea871c912e2e6e693fb2fdf01
E-Nincy <escotonincy10@outlook.es>
2025-06-07 10:56:15 +0800

    add text

--- 02_more-datatypes/02_08_remove_duplicates.py
+++ 02_more-datatypes/02_08_remove_duplicates.py
@@ -3,4 +3,16 @@
 # 1. Convert to a different data type
 # 2. Use a loop and a second list to solve it more manually
 
-list_ = [1, 2, 3, 4, 3, 4, 5]
+# Use a different data type
+my_list_ = [1, 2, 3, 4, 3, 4, 5]
+new_list = list(set(my_list_))
+print(new_list)
+
+# Use a loop and a second list
+my_list_ = [1, 2, 3, 4, 3, 4, 5]
+new_list = []
+for item in my_list_:
+    if item not in new_list:
+        new_list.append(item)
+print(new_list)
+
