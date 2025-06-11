06d1ecb1f6f574c9f03080c3abbe721650757ac3
E-Nincy <escotonincy10@outlook.es>
2025-06-11 19:49:21 +0800

    add text

--- 03_file-input-output/file-counter/analyze.py
+++ 03_file-input-output/file-counter/analyze.py
@@ -1,1 +1,18 @@
-# Use the `csv` module to read in and count the different file types.
+# Use the csv module to read in and count the different file types.
+
+import csv
+from collections import defaultdict
+
+file_type_totals = defaultdict(int)
+
+with open("filecounts.csv", "r", newline="", encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    for row in reader:
+        for ext, count in row.items():
+            if ext != "timestamp":
+                file_type_totals[ext] += int(count)
+
+# Total
+for ext, total in file_type_totals.items():
+    print(f"{ext}: {total}")
