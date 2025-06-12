bbb9a76bba15adbe73668cdae8b45d60aacde519
E-Nincy <escotonincy10@outlook.es>
2025-06-12 15:35:06 +0800

    add solution

--- 03_file-input-output/03_04_pathlib_reader.py
+++ 03_file-input-output/03_04_pathlib_reader.py
@@ -1,3 +1,17 @@
 # Refactor your file counter script to use `pathlib` also for
 # reading and writing to your CSV file. Make sure to handle the
 # path in a way so that you can run the script from anywhere.
+
+from pathlib import Path
+ 
+base_dir = Path(__file__).parent.resolve()
+
+file_path = base_dir / "words.txt"
+output_file_path = base_dir / "new_words.txt"
+
+with file_path.open("r", encoding="utf-8") as file_in:
+    contents = file_in.read()
+
+with output_file_path.open("w", encoding="utf-8") as file_out:
+    file_out.write(contents)
+    print(contents)
