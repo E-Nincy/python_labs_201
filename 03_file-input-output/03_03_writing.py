81f867856cd7f258df4f46418d223b83c0981669
E-Nincy <escotonincy10@outlook.es>
2025-06-12 14:51:51 +0800

    add solution

--- 03_file-input-output/03_03_writing.py
+++ 03_file-input-output/03_03_writing.py
@@ -1,2 +1,16 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+
+from pathlib import Path
+
+from pathlib import Path
+
+words = Path("C:/Users/escot/OneDrive/Escritorio/Codingnomads/python-201-main/03_file-input-output/words.txt")
+reverse_words = Path("C:/Users/escot/OneDrive/Escritorio/Codingnomads/python-201-main/03_file-input-output/words_reverse.txt")
+
+with open(words, "r", encoding="utf-8") as file_in, open(reverse_words, "w", encoding="utf-8") as file_out:
+    lines = file_in.readlines()
+    reverse_lines = lines[::-1]
+    file_out.writelines(reverse_lines)
+
+
