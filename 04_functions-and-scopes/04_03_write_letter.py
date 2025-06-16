dc491365cd63e9599dbe0714016a6e8782b70923
E-Nincy <escotonincy10@outlook.es>
2025-06-16 14:32:12 +0800

    add text

--- 04_functions-and-scopes/04_03_write_letter.py
+++ 04_functions-and-scopes/04_03_write_letter.py
@@ -3,3 +3,12 @@
 # message with the `name` input, as well as a goodbye message that uses
 # the `name` again. Combine that with the input `text` to return a
 # complete `letter`.
+
+def write_letter (name, text):
+    greeting = f"Hello, {name}!"
+    goodbye = f"Goodbye, and see you later Alligator, {name}"
+    letter = f"{greeting}\n{text}\n{goodbye}"
+    return letter
+
+letter = write_letter("Nincy", "I hope you're doing good!")
+print(letter)
