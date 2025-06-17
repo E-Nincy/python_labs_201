5e981a30fa16d59d0750ac8dfd0288866dd15a9a
E-Nincy <escotonincy10@outlook.es>
2025-06-17 16:59:58 +0800

    add text

--- 04_functions-and-scopes/04_10_type_annotations.py
+++ 04_functions-and-scopes/04_10_type_annotations.py
@@ -1,12 +1,12 @@
 # Add type annotations to the three functions shown below.
-
-def multiply(num1, num2):
+ 
+def multiply(num1, num2):  # Type: (int,  int) <- int
     return num1 * num2
 
-def greet(greeting, name):
+def greet(greeting, name):      # Type: (str, str) <- str
     sentence = f"{greeting}, {name}! How are you?"
     return sentence
 
-def shopping_list(*args):
+def shopping_list(*args):       #  Typ: (*str) <- list
     [print(f"* {item}") for item in args]
     return args
