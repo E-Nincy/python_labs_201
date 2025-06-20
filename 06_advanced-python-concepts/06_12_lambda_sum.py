2002fb595caa3d22f57636eba7179de1068614aa
E-Nincy <escotonincy10@outlook.es>
2025-06-20 16:35:14 +0800

    add solution

--- 06_advanced-python-concepts/06_12_lambda_sum.py
+++ 06_advanced-python-concepts/06_12_lambda_sum.py
@@ -1,2 +1,9 @@
 # Write a lambda expression that takes in three numbers
 # and returns the sum of the numbers.
+
+x = 5
+s = 9
+y = 6
+
+sum_lambda = lambda x, s, y: x + s + y
+print(sum_lambda(x, s, y))
