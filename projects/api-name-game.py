71e1758004cc1293ab3032de653935880744c831
E-Nincy <escotonincy10@outlook.es>
2025-06-23 18:52:52 +0800

    add text

--- projects/api-name-game.py
+++ projects/api-name-game.py
@@ -1,1 +1,12 @@
 # Add an API call to your CLI game that assigns a name to your player.
+
+import requests
+
+min_len = 4
+max_len = 6
+URL = f"https://uzby.com/api.php?min={min_len}&max={max_len}"
+
+response = requests.get(URL)
+player_name = response.text.strip()
+
+print(f"Your player name is: {player_name}")
